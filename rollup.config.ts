f19253cce83641f1734d3d4652734ca2f60bf0d8
Alex Saft <fend25@gmail.com>
2022-05-06 14:40:27 +0300

    improved building

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,40 +2,45 @@ import ts from 'rollup-plugin-typescript2'
 import {terser} from 'rollup-plugin-terser'
 import dts from 'rollup-plugin-dts'
 import {RollupOptions} from 'rollup'
+import del from 'rollup-plugin-delete'
+
+
+// "source": "./src/index.ts",
 
 const config: RollupOptions[] = [
   {
     input: './src/index.ts',
     output: [
       {file: "lib/bundle.cjs", format: "cjs", sourcemap: true},
-      {file: "lib/bundle.esm.mjs", format: "es", sourcemap: true},
-      {file: "lib/bundle.esm.min.js", format: "es", plugins: [terser()], sourcemap: true},
+      {file: "lib/bundle.js", format: "es", sourcemap: true},
       {
-        file: "lib/bundle.min.js",
+        file: "lib/bundle.iife.min.js",
         name: "unique",
         format: "iife",
         plugins: [terser()],
         globals: {
-          'util-crypto': 'utilCrypto',
+          '@polkadot/util-crypto': 'polkadotUtilCrypto',
+          '@polkadot/api': 'polkadotApi',
           'web3': 'Web3',
         },
-        sourcemap: true
+        sourcemap: true,
       },
     ],
     plugins: [
+      del({targets: 'lib/*'}),
       ts({
         useTsconfigDeclarationDir: true
       })
     ],
+    external: ['@polkadot/util-crypto', 'web3']
   },
   {
     input: "./lib/dts/src/index.d.ts",
-    output: [{ file: "lib/bundle.d.ts", format: "es" }],
-    plugins: [dts({
-      compilerOptions: {
-        declarationMap: true
-      }
-    })],
+    output: [{file: "lib/bundle.d.ts", format: "es"}],
+    plugins: [
+      dts(),
+      del({hook: "buildEnd", targets: ["./lib/dts", "./lib/bundle.d.ts.map"]}),
+    ],
   },
 ]
 
