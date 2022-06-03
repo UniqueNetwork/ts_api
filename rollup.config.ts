dc46ef6eb92429b1eebbb2a628dc15bdbc5338fb
Alex Saft <fend25@gmail.com>
2022-06-03 12:59:35 +0300

    build improvement

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,15 +1,17 @@
 import ts from 'rollup-plugin-typescript2'
 import {terser} from 'rollup-plugin-terser'
 import dts from 'rollup-plugin-dts'
-import {RollupOptions} from 'rollup'
+import {OutputOptions, RollupOptions} from 'rollup'
 import del from 'rollup-plugin-delete'
 import analyze from 'rollup-plugin-analyzer'
+import gzipPlugin from 'rollup-plugin-gzip'
 
 // This suppresses warning when using custom flag (--analyze)
 const onwarn: RollupOptions['onwarn'] = function onwarn(warning, warn) {
   if (
     warning.code === 'UNKNOWN_OPTION' &&
-    warning.message.startsWith('Unknown CLI flags: analyze')
+    ['analyze', 'mingzip']
+      .some(flag => warning.message.startsWith(`Unknown CLI flags: ${flag}`))
   ) {
     return
   }
@@ -27,13 +29,23 @@ export default (cliOptions: any) => {
   if (cliOptions.analyze) {
     buildPlugins.push(analyze({summaryOnly: true}))
   }
+  if (cliOptions.mingzip) {
+    buildPlugins.push(gzipPlugin({filter: /bundle\.min\.js/}))
+  }
+
+  const output: OutputOptions[] = [
+    {file: "lib/bundle.cjs", format: "cjs", sourcemap: true},
+    {file: "lib/bundle.js", format: "es", sourcemap: true},
+  ]
+  if(cliOptions.mingzip) {
+    output.push({file: "lib/bundle.min.js", format: "umd", name: "unique_tslib", sourcemap: true, plugins: [terser()]})
+  }
 
   const options: RollupOptions[] = [
     {
       input: './src/index.ts',
-      output: [
-        {file: "lib/bundle.cjs", format: "cjs", sourcemap: true},
-        {file: "lib/bundle.js", format: "es", sourcemap: true},
+      output,
+      //[
         // {
         //   file: "lib/bundle.umd.min.js",
         //   name: "unique",
@@ -50,7 +62,7 @@ export default (cliOptions: any) => {
         //   },
         //   sourcemap: true,
         // },
-      ],
+      // ],
 
       // external: ['@polkadot/util-crypto', '@polkadot/api']
       plugins: buildPlugins,
